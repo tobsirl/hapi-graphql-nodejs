f497b807f2aa646f0a3fc60f86810acf310bdb65
Paul Tobin <tobster111@gmail.com>
2018-11-26 19:51:43 +0000

    Added model

--- models/Guitar.js
+++ models/Guitar.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const GuitarSchema = new Schema({
-
-
-
-  
+  name: {
+    type: String,
+    required: true
+  },
+  brand: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Guitar', GuitarSchema);
